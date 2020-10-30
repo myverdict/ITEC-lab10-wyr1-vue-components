227206753ab994333873386d5746a76407955b8c
Sam Vitare <sandhya.palanki@gmail.com>
2020-10-29 20:28:47 -0500

    would you rather with simple vue components

---
+++ wyr/would_you_rather/src/App.vue
@@ -0,0 +1,64 @@
+<!-- this is the parent component of the WouldYouRather.vue -->
+
+<template>
+    <div id="app">
+        <h1>Would you rather...</h1>
+
+        <!-- this is being replaced by the WouldYouRather.vue template -->
+        <!-- a vue component can have dashes, an html element does not have dashes -->
+        <!-- the question, answer1, answer2 are from the props of the child WouldYouRather.vue -->
+        <!-- the wyrQuestion, wyrAnswer1, wyrAnswer2 are from the data of the parent App.vue -->
+        <!-- the answer-changed is from the choiceMade method of the child WouldYouRather.vue -->
+        <!-- the answerChanged method is from the parent App.vue -->
+        <would-you-rather v-bind:question="wyrQuestion"
+                          v-bind:answer1="wyrAnswer1"
+                          v-bind:answer2="wyrAnswer2"
+                          v-on:answer-changed="answerChanged">    <!-- this is from the event changed in the radio buttons -->
+        </would-you-rather>
+
+        <!-- alternate way of writing the above tags/elements -->
+        <!-- <WouldYouRather></WouldYouRather> -->
+
+        <p>{{ userSelectionMessage }}</p>
+    </div>
+</template>
+
+<script>
+    import WouldYouRather from './components/WouldYouRather.vue'
+
+    export default {
+        name: 'App',
+        components: {
+            WouldYouRather
+        },
+        data() {
+            return {
+                wyrQuestion: 'Would you rather have a magic carpet that flies or a see-through submarine?',
+                wyrAnswer1: 'Magic carpet that flies',
+                wyrAnswer2: 'See-through submarine',
+                userSelectionMessage: ''
+            }
+        },
+        methods: {
+            answerChanged(choice) {
+                this.userSelectionMessage = `Thanks! you chose ${choice}`
+            }
+        }
+    }
+</script>
+
+<style>
+    body { background-color: lightseagreen; }
+    #app
+    {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+        background-color: teal;
+        color: white;
+        padding: 20px;
+    }
+</style>
---
+++ wyr/would_you_rather/src/components/WouldYouRather.vue
@@ -0,0 +1,58 @@
+<!-- this is a child component of the App.vue -->
+
+<template>
+    <div class="wyr">
+        <h2>Please make your choice!</h2>
+
+        <!-- Would you rather question -->
+        <h3>{{ question }}</h3>
+
+        <div id="ans">
+        <!-- radio buttons for the answers -->
+        <input type="radio" v-model="choice" v-bind:value="answer1" v-on:change="choiceMade">
+        <label>{{ answer1 }}</label>
+
+        <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="choiceMade">
+        <label>{{ answer2 }}</label>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'WouldYouRather',
+        // The App.vue parent will provide these values here (never modify a prop in a component)
+        props: {
+            question: String,
+            answer1: String,
+            answer2: String,
+        },
+        // for the radio button choices: data is a function that returns an object
+        data() {
+            return {
+                choice: ''
+            }
+        },
+        methods: {
+            // create a choiceMade method here
+            choiceMade() {
+                this.$emit("answer-changed", this.choice)
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3 { margin: 40px 0 0; }
+    .wyr
+    {
+        border: 2px solid black;
+        background-color: greenyellow;
+        margin: 10px;
+        color: black;
+        padding: 20px;
+    }
+    #ans { padding: 20px; }
+    label { padding-right: 10px; }
+</style>
